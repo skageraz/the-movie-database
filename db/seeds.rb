e6e590ef6c154f08d7fdcebf10ab5642d400f2e8
skageraz <skageraz@gmail.com>
2018-08-01 17:00:35 +0200

    final

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,15 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 Movie.destroy_all
-movie1 = Movie.create(title: 'Terminator', release_year: '1984', director:'James Cameron')
-movie2 = Movie.create(title: 'Predator', release_year: '1987', director:'John Mc Tiernan')
-movie3 = Movie.create(title: 'Last Action Hero', release_year: '1993', director:'John Mc Tiernan')
-movie4 = Movie.create(title: 'Total Recall', release_year: '1990', director:'Paul Verhoeven')
-movie5 = Movie.create(title: 'Running Man', release_year: '1987', director:'Paul Michael Glaser')
+Director.destroy_all
+director1 = Director.create(id:1, first_name: 'James ', last_name: 'Cameron')
+director2 = Director.create(id:2, first_name: 'John ', last_name: 'Mc Tiernan')
+director3 = Director.create(id:3, first_name: 'Paul ', last_name: 'Verhoeven')
+director4 = Director.create(id:4, first_name: 'Paul ', last_name: 'Michael Glaser')
+
+movie1 = Movie.new(title: 'Terminator', release_year: '1984', director_id: 1) #director:'James Cameron')
+movie1.save!
+movie2 = Movie.create(title: 'Predator', release_year: '1987', director_id:2) #director:'John Mc Tiernan')
+movie3 = Movie.create(title: 'Last Action Hero', release_year: '1993', director_id:2) #director:'John Mc Tiernan')
+movie4 = Movie.create(title: 'Total Recall', release_year: '1990', director_id:3) #director:'Paul Verhoeven')
+movie5 = Movie.create(title: 'Running Man', release_year: '1987', director_id:4) #director:'Paul Michael Glaser')
